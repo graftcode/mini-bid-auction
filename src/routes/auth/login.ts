a0b4ee703036ebabf02db3d52e13a85988edfcdb
graftcode <graftcode@outlook.com>
2022-04-19 00:09:13 +0100

    login route

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,9 +1,45 @@
 import { Router as expressRouter } from "express";
+import { compare } from "bcryptjs";
+import User from "../../models/User";
+
+import { IUser } from "../../models/User";
+import { loginValidation } from "../../validation/loginValidation";
 
 const router = expressRouter();
 
-router.post("/login", (req, res) => {
-  res.send("logged in");
+interface IrequestBody {
+  email: string;
+  password: string;
+}
+
+router.post("/login", async (req, res) => {
+  const { email, password }: IrequestBody = req.body;
+
+  const user: IUser | null = await User.findOne({
+    email,
+  });
+
+  const { error } = loginValidation.validate({
+    email,
+    password,
+  });
+
+  if (error) return res.send({ message: error.details[0].message });
+
+  if (!user) {
+    return res.send({
+      message:
+        "Sorry, either you have entered wrong information or we cannot find acount",
+    });
+  }
+
+  const passwordIsMatch = await compare(password, user.password);
+
+  if (passwordIsMatch) {
+    return res.send({
+      message: "Successfully logged in",
+    });
+  }
 });
 
 export default router;
