414a571bc4c502352c6ba79119f1e57831696ce8
graftcode <graftcode@outlook.com>
2022-04-18 05:08:47 +0100

    wip - signup route with validation and error handling

--- src/routes/auth/signup.ts
+++ src/routes/auth/signup.ts
@@ -1,9 +1,42 @@
 import { Router as expressRouter } from "express";
 
+import UserModel from "../../models/User";
+import { applyCorrectCasing } from "../../utils/applyCorrectCasing";
+import { signupValidation } from "../../validation/signupValidation";
+
 const router = expressRouter();
 
-router.post("/signup", (req, res) => {
-  res.send("signed in");
+router.post("/signup", async (req, res) => {
+  const { email, password, username, fullname } = req.body;
+
+  const userExists = await UserModel.findOne({ email });
+
+  if (userExists) {
+    res.send({ message: "User already exists" });
+    return;
+  }
+
+  const { error } = signupValidation.validate({
+    email,
+    password,
+    username,
+    fullname,
+  });
+
+  if (error) return res.send({ message: error.details[0].message });
+
+  try {
+    const newUser = await UserModel.create({
+      email: email.toLowerCase(),
+      password,
+      username: username.toLowerCase(),
+      fullname: applyCorrectCasing(fullname),
+    });
+
+    res.send({ message: "Successfully signed up", data: newUser });
+  } catch (error) {
+    res.send({ message: error });
+  }
 });
 
 export default router;
