24a53106505f3734fb019a82546e7eb2f0d7e2e4
graftcode <graftcode@outlook.com>
2022-04-21 22:59:45 +0100

    add verifytoken logic

--- src/validation/verifyToken.ts
+++ src/validation/verifyToken.ts
@@ -1,1 +1,19 @@
-// add verifytoken middleware for login
+import { Request, Response, NextFunction } from "express";
+import { verify, JwtPayload } from "jsonwebtoken";
+
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  const authToken = req.header("auth-token") as string;
+  const secret = process.env.TOKEN_SECRET as string;
+
+  if (!authToken) res.status(401).send({ message: "Access denied." });
+
+  try {
+    const { _id }: JwtPayload["_id"] = verify(authToken, secret);
+    res.locals = { _id };
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid token" });
+  }
+};
+
+export default verifyToken;
