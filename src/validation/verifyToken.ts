137f895450e4fa199c248818392ca6731a2857b0
graftcode <graftcode@outlook.com>
2022-04-25 21:09:07 +0100

    add return statement

--- src/validation/verifyToken.ts
+++ src/validation/verifyToken.ts
@@ -5,11 +5,12 @@ const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   const authToken = req.header("auth-token") as string;
   const secret = process.env.TOKEN_SECRET as string;
 
-  if (!authToken) res.status(401).send({ message: "Access denied." });
+  if (!authToken) return res.status(401).send({ message: "Access denied." });
 
   try {
     const { _id }: JwtPayload["_id"] = verify(authToken, secret);
     res.locals = { _id };
+    console.log("verifyToken here")
     next();
   } catch (error) {
     return res.status(401).send({ message: "Invalid token" });
