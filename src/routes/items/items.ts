f8587bb78d28799705ef46ff73bb22825ad9c56d
graftcode <graftcode@outlook.com>
2022-04-25 23:51:45 +0100

    see items on platform (excludes items sold by user)

--- src/routes/items/items.ts
+++ src/routes/items/items.ts
@@ -1,11 +1,27 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import verifyToken from "../../validation/verifyToken";
+
+import ItemModel from "../../models/Items";
 
 const router = Router();
 
-router.get("/items", verifyToken, (req, res) => {
-  console.log("res.locals: ", res.locals);
-  res.send("this is the items page");
+router.get("/items", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const itemsForSale = await ItemModel.find({
+      status: "Open",
+    })
+      .where("seller_id")
+      .ne(res.locals._id); //ensures we don't get back items we placed on platform
+
+    if (itemsForSale) {
+      return res.send({
+        message: "Items stil up for sale",
+        data: itemsForSale,
+      });
+    }
+  } catch (error) {
+    return res.send({ message: error });
+  }
 });
 
 export default router;
