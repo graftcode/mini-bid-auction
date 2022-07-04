2c9c9c678aa12450dd5b4b1157b953adf279b716
graftcode <graftcode@outlook.com>
2022-07-04 23:36:06 +0100

    implement routes and protected route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
-import React, { Context, useState } from "react";
+import React, { useState } from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom/";
 
 import Auth from "./Pages/Auth/Auth";
+import AuctionListings from "./Pages/Listings/AuctionListings";
 import "./App.css";
 import { AuthContext } from "./Contexts/AuthContext";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 function App() {
   const [authToken, setAuthToken] = useState<any>(null);
@@ -10,7 +13,15 @@ function App() {
     <AuthContext.Provider value={{ authToken, setAuthToken }}>
       <div className="App">
         <h1>APP COMponent</h1>
-        <Auth />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Auth />} />
+
+            <Route element={<ProtectedRoutes authToken={authToken} />}>
+              <Route path="/listings" element={<AuctionListings />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </AuthContext.Provider>
   );
